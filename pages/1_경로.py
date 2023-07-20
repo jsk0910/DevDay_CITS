920486c4a90c6d862809b5cdefa4c46397382d3a
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-21 03:05:16 +0900

    Update 1_경로.py

--- pages/1_경로.py
+++ pages/1_경로.py
@@ -117,7 +117,7 @@ if 'r' not in st.session_state or address != st.session_state.old_address:
   for i in range(3):
     folium.Marker(location = [min.iloc[i]['위도'], min.iloc[i]['경도']],
                   popup = folium.Popup(f'{min.iloc[i]['의료기관명']}\n전화번호: {min.iloc[i]['전화번호']}\n,
-                  width=150),
+                  width=250),
                   tooltip = min.iloc[i]['의료기관명'],
                   icon=folium.Icon(color='blue', icon='plus')
                  ).add_to(r)
