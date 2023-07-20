103d5d491ebeda4e84a29971bf23da777d2e03e2
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-21 02:33:40 +0900

    Update 1_경로.py

--- pages/1_경로.py
+++ pages/1_경로.py
@@ -91,16 +91,18 @@ orig = st.session_state.orig
 
 style = {'color': '#1A19AC', 'weight':'1'}
 min = df_hospital_distance.sort_values(by="distance")
-st.write(min)
+st.session_state.min = min
 
 if 'departments' in st.session_state:
   departments = st.session_state.departments
-  depart = ""
-  for d in departments:
-    depart += d
-    depart += "&"
-  min = min[min['진료과목'].str.contains(depart[:-1])]
-  st.write(min)
+  try:
+    depart = ""
+    for d in departments:
+      depart += d
+      depart += "&"
+    min = min[min['진료과목'].str.contains(depart[:-1])]
+  except:
+    min = st.session_state.min
 
 if 'r' not in st.session_state or address != st.session_state.old_address:
   orig = st.session_state.orig
