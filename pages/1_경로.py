bd492a1ae8117e6784ad695de2cf8681fa074662
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-21 01:07:03 +0900

    Update 1_경로.py

--- pages/1_경로.py
+++ pages/1_경로.py
@@ -49,6 +49,8 @@ def routeHospital(G, orig, destX, destY):
   # fig, ax = ox.plot_graph(G, node_size=0, edge_linewidth=0.5)
 
   dest = ox.distance.nearest_nodes(G, X=destX, Y=destY)
+  st.write(orig)
+  st.write(dest)
   route = ox.shortest_path(G, orig, dest, weight="travel_time")
   r = ox.plot_route_folium(G, route, popup_attribute='length')
   return r
