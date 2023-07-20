20cf6da21a968b9d72eb8a8b14fb457be98b79dc
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-21 02:25:17 +0900

    Update 1_경로.py

--- pages/1_경로.py
+++ pages/1_경로.py
@@ -91,6 +91,7 @@ orig = st.session_state.orig
 
 style = {'color': '#1A19AC', 'weight':'1'}
 min = df_hospital_distance.sort_values(by="distance")
+departments = st.session_state.departments
 
 if 'r' not in st.session_state or address != st.session_state.old_address:
   orig = st.session_state.orig
