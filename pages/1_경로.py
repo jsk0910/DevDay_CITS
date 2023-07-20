d966d2409cb3990cc745ecb27b36aca11e885616
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-21 02:15:22 +0900

    Update 1_경로.py

--- pages/1_경로.py
+++ pages/1_경로.py
@@ -51,8 +51,8 @@ def routeHospital(G, orig, dest):
   route3 = ox.shortest_path(G, orig, dest3, weight="travel_time")
   
   r = ox.plot_route_folium(G, route1, popup_attribute='length', zoom=13, route_color="orange")
-  r = ox.plot_route_folium(G, route2, route_map=r, popup_attribute='length', edge_color="orange")
-  r = ox.plot_route_folium(G, route3, route_map=r, popup_attribute='length')
+  r = ox.plot_route_folium(G, route2, route_map=r, popup_attribute='length', zoom=13, edge_color="orange")
+  r = ox.plot_route_folium(G, route3, route_map=r, popup_attribute='length', zoom=13)
   return r
 
 htmlTitle = """
