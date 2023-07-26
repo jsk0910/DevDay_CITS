1065944221a50e53fee06161813c41732923f3b0
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-26 15:18:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,9 @@ def readData():
 def makeGraph(item, G):
   # 진료과 + 응급도 등급으로 도출되도록 변경 필요
   # 그래프에서 관련된 여러 과가 나오도록 변경 필요
+  st.write(item['description'].split(', ')[2] + ' ' + str(item['level']))
   G.add_edge(item['firstCode'] + item['secondCode'] + item['thirdCode'] + item['fourthCode'], item['description'].split(', ')[2] + ' ' + str(item['level']))
+  return G
 
 def getDepartment(possible_departments:list):
   mergeCode = st.session_state.mergeCode
