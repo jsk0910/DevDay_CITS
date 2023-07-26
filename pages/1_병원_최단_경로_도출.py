12e601232abc6972f4f58858fca2fb3abe90f470
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-27 00:09:37 +0900

    Update 1_병원_최단_경로_도출.py

--- pages/1_병원_최단_경로_도출.py
+++ pages/1_병원_최단_경로_도출.py
@@ -151,7 +151,7 @@ if 'r' not in st.session_state or address != st.session_state.old_address:
   for i in range(3):
     folium.Marker(location = [min.iloc[i]['위도'], min.iloc[i]['경도']],
                   popup = folium.Popup(f"{min.iloc[i]['의료기관명']}\n전화번호: {min.iloc[i]['전화번호']}\n",
-                  width=250),
+                  max_width=300),
                   tooltip = min.iloc[i]['의료기관명'],
                   icon=folium.Icon(color='blue', icon='plus')
                  ).add_to(r)
