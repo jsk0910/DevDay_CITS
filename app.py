a60f299a4a9012b3e609693d80d86c2954b8daf2
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-26 16:54:36 +0900

    Update app.py

--- app.py
+++ app.py
@@ -193,7 +193,7 @@ def main():
   for i in st.session_state.possible_departments:
     emerCode = int(i.split('|')[1])
     html2 = f"""
-            <tr class="{'table-warning' if emerCode > 3 else 'table-danger'}">
+            <tr class="{"table-warning" if emerCode > 3 else "table-danger"}">
               <th class="row">i.split('|')[0]</th>
               <td>i.split('|')[2]</td>
               <td>i.split('|')[1]</td>
