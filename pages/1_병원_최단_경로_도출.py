a39d50216e56a5654843cc9922e30fc65fa64d6f
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-27 00:22:10 +0900

    Update 1_병원_최단_경로_도출.py

--- pages/1_병원_최단_경로_도출.py
+++ pages/1_병원_최단_경로_도출.py
@@ -138,13 +138,14 @@ if 'departments' in st.session_state:
     min = st.session_state.min
 
 if 'r' not in st.session_state or address != st.session_state.old_address:
+  newline = '\n'
   orig = st.session_state.orig
   r = None
   with st.spinner('경로 계산중...'):
     r = routeHospital(G_map, orig, [[min.iloc[0]['경도'], min.iloc[0]['위도']],[min.iloc[1]['경도'], min.iloc[1]['위도']], [min.iloc[2]['경도'], min.iloc[2]['위도']]])
   for _, row in df_hospital.iterrows():
     folium.Marker(location = [row['위도'], row['경도']],
-            popup=folium.Popup(f"의료기관명: {row['의료기관명']}{'\n'}전화번호: {row['전화번호']}", max_width=300),
+            popup=folium.Popup(f"의료기관명: {row['의료기관명']}{newline}전화번호: {row['전화번호']}", max_width=300),
             tooltip=row['의료기관명'],
             icon=folium.Icon(color='red',icon='plus')
           ).add_to(r)
