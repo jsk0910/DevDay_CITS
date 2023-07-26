c55888b3491e803ba2c6c5c8465daa594e37bfe6
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-07-26 13:51:32 +0900

    Update 1_경로.py

--- pages/1_경로.py
+++ pages/1_경로.py
@@ -139,7 +139,7 @@ if 'departments' in st.session_state:
 if 'r' not in st.session_state or address != st.session_state.old_address:
   orig = st.session_state.orig
   r = None
-  with st.spinner():
+  with st.spinner('경로 계산중...'):
     r = routeHospital(G_map, orig, [[min.iloc[0]['경도'], min.iloc[0]['위도']],[min.iloc[1]['경도'], min.iloc[1]['위도']], [min.iloc[2]['경도'], min.iloc[2]['위도']]])
   for _, row in df_hospital.iterrows():
     folium.Marker(location = [row['위도'], row['경도']],
