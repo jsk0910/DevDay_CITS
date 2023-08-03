8f3b169ecea4774b88a3c941b7beae2bc5f21c1a
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-08-03 12:11:54 +0900

    Update database.py

--- src/database.py
+++ src/database.py
@@ -2,8 +2,12 @@
 import certifi
 from pymongo import MongoClient
 
+# connection to database
 def connectDB(password):
   client = MongoClient(f'mongodb+srv://admin:{password}@cits.wpznlau.mongodb.net/?retryWrites=true&w=majority', tlsCAFile=certifi.where())
   db = client.cits
   return db
   
+# use hospital collection Create
+def createHospitalInformation(hospitalName):
+  pass
