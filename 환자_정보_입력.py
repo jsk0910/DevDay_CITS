a365070648c0674cc9a1d7c6d0d1f1216d0ab8ff
SangKyun Jeon <sangkyun.jeon@gmail.com>
2023-08-07 14:14:28 +0900

    Update 환자_정보_입력.py

--- 환자_정보_입력.py
+++ 환자_정보_입력.py
@@ -222,7 +222,7 @@ def main():
                                     model=model,
                                     messages=messages
                                 )
-                                st.write(response)
+                                st.write('response: '+response)
                                 gpt_answer.append(response['choices'][0]['message']['content'])
                             if j == 3:
                                 break
